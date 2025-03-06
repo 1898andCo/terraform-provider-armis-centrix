faa8e761783ea19a78b14839f0b6371799609f0f
RoseSecurity <michaeljrosenfeld22@gmail.com>
2025-03-06 14:32:32 -0500

    feat: add provider tests for the user data source

---
+++ internal/provider/user_data_source_test.go
@@ -0,0 +1,40 @@
+// Copyright (c) 1898 & Co.
+// SPDX-License-Identifier: Apache-2.0
+
+package provider_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAcc_UserDataSource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccUserDataSourceConfig(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.armis_user.test", "users.#"),
+					resource.TestCheckResourceAttrSet("data.armis_user.test", "users.0.id"),
+					resource.TestCheckResourceAttrSet("data.armis_user.test", "users.0.name"),
+					resource.TestCheckResourceAttrSet("data.armis_user.test", "users.0.email"),
+					resource.TestCheckResourceAttrSet("data.armis_user.test", "users.0.is_active"),
+					resource.TestCheckResourceAttrSet("data.armis_user.test", "users.0.role_assignment.#"),
+					resource.TestCheckResourceAttrSet("data.armis_user.test", "users.0.role_assignment.0.name.#"),
+					resource.TestCheckResourceAttrSet("data.armis_user.test", "users.0.two_factor_authentication"),
+					resource.TestCheckResourceAttrSet("data.armis_user.test", "users.0.username"),
+				),
+			},
+		},
+	})
+}
+
+func testAccUserDataSourceConfig() string {
+	return fmt.Sprintf(`
+data "armis_user" "test" {}
+`)
+}
