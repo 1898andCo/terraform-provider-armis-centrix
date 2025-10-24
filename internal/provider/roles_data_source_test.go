202252f33b65d4685793e66e812a232247009889
RoseSecurity <michael@rosesecurity.dev>
2025-10-24 13:27:23 -0400

    chore: update tests

--- internal/provider/roles_data_source_test.go
+++ internal/provider/roles_data_source_test.go
@@ -17,24 +17,24 @@ func TestAcc_RoleDataSource(t *testing.T) {
 			{
 				Config: testAccRoleDataSourceConfig(),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "name"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "role_id"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "vipr_role"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "permissions.advanced_permissions.all"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "permissions.advanced_permissions.behavioral.all"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "permissions.advanced_permissions.behavioral.application_name"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "permissions.advanced_permissions.behavioral.host_name"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "permissions.advanced_permissions.behavioral.service_name"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "permissions.advanced_permissions.device.all"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "permissions.advanced_permissions.device.device_names"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "permissions.advanced_permissions.device.ip_addresses"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "permissions.advanced_permissions.device.mac_addresses"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "permissions.advanced_permissions.device.phone_numbers"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "permissions.alert.all"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "permissions.alert.manage.all"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "permissions.alert.manage.resolve"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "permissions.alert.manage.whitelist_devices"),
-					resource.TestCheckResourceAttrSet("data.armis_role.test", "permissions.alert.read"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "name"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "role_id"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "vipr_role"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "permissions.advanced_permissions.all"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "permissions.advanced_permissions.behavioral.all"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "permissions.advanced_permissions.behavioral.application_name"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "permissions.advanced_permissions.behavioral.host_name"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "permissions.advanced_permissions.behavioral.service_name"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "permissions.advanced_permissions.device.all"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "permissions.advanced_permissions.device.device_names"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "permissions.advanced_permissions.device.ip_addresses"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "permissions.advanced_permissions.device.mac_addresses"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "permissions.advanced_permissions.device.phone_numbers"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "permissions.alert.all"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "permissions.alert.manage.all"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "permissions.alert.manage.resolve"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "permissions.alert.manage.whitelist_devices"),
+					resource.TestCheckResourceAttrSet("data.armis_roles.test", "permissions.alert.read"),
 				),
 			},
 		},
@@ -43,7 +43,7 @@ func TestAcc_RoleDataSource(t *testing.T) {
 
 func testAccRoleDataSourceConfig() string {
 	return `
-		data "armis_role" "test" {
+		data "armis_roles" "test" {
 			name = "Admin"
 		}
 	`
