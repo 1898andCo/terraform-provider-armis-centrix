d3cd2aff689e2ada6b73cb94c6d9e46d77bc187b
Musthaq Ahamed <amusthaq56@gmail.com>
2025-11-10 22:21:52 +0530

    removing provider config in examples

--- examples/data-sources/lists/lists.tf
+++ examples/data-sources/lists/lists.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    armis = {
-      source = "1898andCo/armis-centrix"
-    }
-  }
-}
-
-# Configure provider elsewhere or via environment variables
-# provider "armis" {}
-
 data "armis_lists" "all" {}
 
 output "armis_lists_names" {
